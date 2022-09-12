e86d14ee1d49a2db1d69277f7a0a1ff4a0a68d96
simasnau <simasnau@gmail.com>
2022-09-12 18:10:42 +0300

    Pridėti mygtukai animacijos valdymui

--- script.js
+++ script.js
@@ -3,16 +3,51 @@ var WIDTH;
 var HEIGHT;
 var ctx;
 var canvas;
-var atstumasTarpRatu = 20;
-var ratoR1 = 30;
-var ratoR2 = 15;
-var distance = 0;
-var ratoPozicijaX = 150;
+var atstumasTarpRatu;
+var ratoR1;
+var ratoR2;
+var distance;
+var ratoPozicijaX;
 var zemesAukstisY = 150;
-var stipinuKiekis1 = 6;
-var stipinuKiekis2 = 4;
+var stipinuKiekis1;
+var stipinuKiekis2;
+var intervalId;
+
+var ratas1Input = document.getElementById("ratas1Input");
+ratas1Input.value = 30;
+
+var ratas2Input = document.getElementById("ratas2Input");
+ratas2Input.value = 15;
+
+var ratas1StipinaiInput = document.getElementById("ratas1StipinaiInput");
+ratas1StipinaiInput.value = 6;
+
+var ratas2StipinaiInput = document.getElementById("ratas2StipinaiInput");
+ratas2StipinaiInput.value = 4;
+
+var atstumasInput = document.getElementById("atstumas");
+atstumasInput.value = 20;
+
+var startButton = document.getElementById("startButton");
+startButton.addEventListener("click", onStart);
+
+function onStart() {
+	clearInterval(intervalId);
+	intervalId = init();
+	clear();
+}
 
 function init() {
+	ratoPozicijaX = 150;
+	distance = 0;
+	ratoR1 = parseInt(ratas1Input.value);
+	ratoR2 = parseInt(ratas2Input.value);
+
+	stipinuKiekis1 = parseInt(ratas1StipinaiInput.value);
+	stipinuKiekis2 = parseInt(ratas2StipinaiInput.value);
+
+	atstumasTarpRatu = parseInt(atstumasInput.value);
+
 	canvas = document.getElementById("canvas");
 	ctx = canvas.getContext("2d");
 	WIDTH = canvas.width;
@@ -49,7 +84,6 @@ function clear() {
 	ctx.clearRect(0, 0, WIDTH, HEIGHT);
 }
 
-
 function ratas(xc, yc, r, stipinuKiekis) {
 	var apsisukimaiRadianais = distance / r;
 
@@ -85,5 +119,3 @@ function draw() {
 	distance += dx;
 	ratoPozicijaX += dx;
 }
-
-init();
