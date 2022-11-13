acb828bf33e62fce867eaa491c1dd1b837002359
simasnau <simasnau@gmail.com>
2022-11-13 20:25:41 +0200

    Prideta funkcija figuru padejimui, karaliaus, deziu generavimas

--- 2LAB/script.js
+++ 2LAB/script.js
@@ -13,8 +13,8 @@ $(function () {
 
 	// create a camera, which defines where we're looking at.
 	const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 500);
-	camera.position.x = 120;
-	camera.position.y = 60;
+	camera.position.x = 0;
+	camera.position.y = 100;
 	camera.position.z = 180;
 	camera.lookAt(new THREE.Vector3(0, 0, 0));
 
@@ -26,7 +26,7 @@ $(function () {
 
 	// create the ground plane
 	const planeGeometry = new THREE.PlaneGeometry(180, 180);
-	const texture = THREE.ImageUtils.loadTexture("textures/ChessBoard.png");
+	const texture = THREE.ImageUtils.loadTexture("textures/ChessBoardSvg.svg");
 	const textureMaterial = new THREE.MeshBasicMaterial({ map: texture });
 	const plane = new THREE.Mesh(planeGeometry, textureMaterial);
 
@@ -48,8 +48,6 @@ $(function () {
 	// add the output of the renderer to the html element
 	$("#WebGL-output").append(renderer.domElement);
 
-	let step = 0;
-
 	const controls = new (function () {
 		this.fov = 75;
 	})();
@@ -58,25 +56,24 @@ $(function () {
 	gui.add(controls, "fov", 25, 100);
 
 	const firstMesh = createChessFigureMesh();
-	firstMesh.position.set(-50, -50, -50);
+	moveChessPieceOnBoard(firstMesh, 'C4');
 	scene.add(firstMesh);
 
 	const secondMesh = createChessFigureMesh();
-	secondMesh.position.set(0, 18, -50);
+	moveChessPieceOnBoard(secondMesh, 'B2');
 	scene.add(secondMesh);
+
+	// const pawn = createPawnBox();
+	// moveChessPieceOnBoard(pawn, 'A2');
+	// scene.add(pawn);
+
 	const camControl = new THREE.TrackballControls(camera, renderer.domElement);
 	render();
 
 	function render() {
 		stats.update();
 
-		let x = 10 + 50 * Math.sin(step);
-		firstMesh.position = new THREE.Vector3(x, 18, 50);
-
-		step += 0.01;
 		camera.fov = controls.fov;
-		camera.lookAt(new THREE.Vector3(0, 0, 0));
-
 		camera.updateProjectionMatrix();
 
 		camControl.update();
@@ -86,16 +83,20 @@ $(function () {
 	}
 	function createChessFigureMesh() {
 		const pointsX = [
-			250, 240, 230, 220, 210, 200, 200, 205, 214, 230, 220, 207, 210, 218, 228, 226, 224, 221, 217, 209, 198, 188, 184, 182, 190,
-			180, 175, 179, 172, 173, 250,
+			240, 202, 214, 225, 210,
+			200, 225, 225, 220, 215,
+			208, 205, 197, 186, 190,
+			169, 168, 172, 161, 160,
 		];
 		const pointsY = [
-			34, 46, 58, 70, 82, 94, 106, 118, 130, 142, 154, 166, 178, 190, 202, 214, 226, 238, 250, 262, 274, 286, 298, 310, 322, 334,
-			346, 358, 370, 382, 382,
+			85, 97, 124, 145, 155,
+			175, 180, 205, 235, 265,
+			283, 295, 307, 316, 325,
+			334, 343, 352, 361, 370,
 		];
 		const points = [];
 		for (let i = 0; i < pointsX.length; i++) {
-			points.push(new THREE.Vector3(25 - pointsX[i] / 10, 0.01, (pointsY[30] - pointsY[i] - 174) / 10));
+			points.push(new THREE.Vector3(25-pointsX[i]/10, 0.01, (pointsY[pointsX.length - 1]-pointsY[i]-174)/10));
 		}
 
 		const latheGeometry = new THREE.LatheGeometry(points, Math.ceil(12), 0, 2 * Math.PI);
@@ -106,6 +107,100 @@ $(function () {
 		const mesh = new THREE.Mesh(latheGeometry, meshMaterial);
 		mesh.rotateX(-Math.PI / 2);
 		return mesh;
+	}
+
+	// function createPawnBox() {
+	// 	// create box
+	// 	const width = 10;
+	// 	const height = 2;
+	// 	const depth = 10;
+	//
+	// 	const boxGeometry = new THREE.BoxGeometry(width, height, depth);
+	// 	const boxMaterial = new THREE.MeshLambertMaterial({ color: 0xff0000 });
+	//
+	// 	// stack of boxes
+	// 	// for (let i = 0; i < 5; i++) {
+	// 		const box = new THREE.Mesh(boxGeometry, boxMaterial);
+	// 		box.castShadow = true;
+	// 		box.position.y = (0.5 + 2 * 1.2) * height;
+	// 		// scene.add(box);
+	// 	// }
+	//
+	// 	// const latheGeometry = new THREE.LatheGeometry(points, Math.ceil(12), 0, 2 * Math.PI);
+	// 	//
+	// 	// const meshMaterial = new THREE.MeshLambertMaterial({color: 0x444444});
+	// 	// meshMaterial.side = THREE.DoubleSide;
+	// 	//
+	// 	// const mesh = new THREE.Mesh(latheGeometry, meshMaterial);
+	// 	// mesh.rotateX(-Math.PI / 2);
+	// 	// return mesh;
+	// 	return box;
+	// }
+
+	function moveChessPieceOnBoard(chessPiece, positionString) {
+		if (positionString.length !== 2) {
+			throw new Error("Wrong length passed")
+		}
+		let x = -67
+		let z = 64
+		const gap = 19;
+
+		let xMultiplier = 1;
+		let zMultiplier = 1;
+
+		positionString = positionString.toUpperCase();
+		switch (positionString.charAt(0)) {
+			case 'B':
+				xMultiplier = 2;
+				break;
+			case 'C':
+				xMultiplier = 3;
+				break;
+			case 'D':
+				xMultiplier = 4;
+				break;
+			case 'E':
+				xMultiplier = 5;
+				break;
+			case 'F':
+				xMultiplier = 6;
+				break;
+			case 'G':
+				xMultiplier = 7;
+				break;
+			case 'H':
+				xMultiplier = 8;
+				break;
+		}
+
+		switch (positionString.charAt(1)) {
+			case '2':
+				zMultiplier = 2;
+				break;
+			case '3':
+				zMultiplier = 3;
+				break;
+			case '4':
+				zMultiplier = 4;
+				break;
+			case '5':
+				zMultiplier = 5;
+				break;
+			case '6':
+				zMultiplier = 6;
+				break;
+			case '7':
+				zMultiplier = 7;
+				break;
+			case '8':
+				zMultiplier = 8;
+				break;
+		}
+
+		x = x + (xMultiplier - 1) * gap;
+		z = z - (zMultiplier - 1) * gap;
+
+		chessPiece.position.set(x, 18, z);
 	}
 
 	function initStats() {
