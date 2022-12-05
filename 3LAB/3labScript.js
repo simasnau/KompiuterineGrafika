6e4d6e8506987a6ae250f3e14d667acc4af0e122
simasnau <simasnau@gmail.com>
2022-12-05 17:36:06 +0200

    refcatored

--- 3LAB/3labScript.js
+++ 3LAB/3labScript.js
@@ -1,16 +1,19 @@
 import * as THREE from "https://unpkg.com/three@0.123.0/build/three.module.js";
-import { OrbitControls } from "https://unpkg.com/three@0.123.0/examples/jsm/controls/OrbitControls.js";
+import { TrackballControls } from "https://unpkg.com/three@0.123.0/examples/jsm/controls/TrackballControls.js";
 import { ConvexGeometry } from "https://unpkg.com/three@0.123.0/examples/jsm/geometries/ConvexGeometry.js";
 import Stats from "https://unpkg.com/three@0.123.0/examples/jsm/libs/stats.module";
 
 const stats = initStats();
 
+const r = 5;
+const R = 7.5;
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 1000);
-camera.position.x = -30;
+camera.position.x = -20;
 camera.position.y = 40;
-camera.position.z = 50;
+camera.position.z = 40;
 camera.lookAt(scene.position);
 
 const renderer = new THREE.WebGLRenderer();
@@ -18,7 +21,7 @@ renderer.setClearColor(0xeeeeee, 1.0);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 $("#WebGL-output").append(renderer.domElement);
-const orbitControls = new OrbitControls(camera, renderer.domElement);
+const trackballControls = new TrackballControls(camera, renderer.domElement);
 
 const axes = new THREE.AxesHelper(50);
 scene.add(axes);
@@ -26,35 +29,32 @@ scene.add(axes);
 const ambientLight = new THREE.AmbientLight(0xcccccc, 1);
 scene.add(ambientLight);
 
-const r = 5;
-const R = 7.5;
-const pointsCount = 10000;
-
-const generatedPoints = generatePoints(R, r, pointsCount);
-
+const generatedPoints = generatePoints(R, r);
 const torusPoints = filterPoints(R, r, generatedPoints);
 
 const geometry = new ConvexGeometry(torusPoints);
 
-geometry.faceVertexUvs[0] = [];
-
-const faces = geometry.faces;
-
-let u1, u2, u3;
 const s = 4;
-for (const face of faces) {
-	const v1 = geometry.vertices[face.a],
-		v2 = geometry.vertices[face.b],
-		v3 = geometry.vertices[face.c];
+geometry.faceVertexUvs[0] = [];
+for (const face of geometry.faces) {
+	const v1 = geometry.vertices[face.a];
+	const v2 = geometry.vertices[face.b];
+	const v3 = geometry.vertices[face.c];
 
-	u1 = calcU(v1.x, v1.z, s);
-	u2 = calcU(v2.x, v2.z, s);
-	u3 = calcU(v3.x, v3.z, s);
+	let u1 = calcU(v1.x, v1.z, s);
+	let u2 = calcU(v2.x, v2.z, s);
+	let u3 = calcU(v3.x, v3.z, s);
 
 	if (u1 > 0.9 * s || u2 > 0.9 * s || u3 > 0.9 * s) {
-		if (u1 < s * 0.8) u1 += s;
-		if (u2 < s * 0.8) u2 += s;
-		if (u3 < s * 0.8) u3 += s;
+		if (u1 < s * 0.8) {
+			u1 += s;
+		}
+		if (u2 < s * 0.8) {
+			u2 += s;
+		}
+		if (u3 < s * 0.8) {
+			u3 += s;
+		}
 	}
 
 	geometry.faceVertexUvs[0].push([
@@ -65,29 +65,29 @@ for (const face of faces) {
 }
 geometry.uvsNeedUpdate = true;
 
+const wireFrameMaterial = new THREE.MeshBasicMaterial({ color: 0x969100 });
+wireFrameMaterial.wireframe = true;
+
 const texture = new THREE.TextureLoader().load("textures/texture.jpg");
 texture.wrapS = THREE.RepeatWrapping;
 
 const material = new THREE.MeshBasicMaterial({ map: texture });
 
-const wireFrameMaterial = new THREE.MeshBasicMaterial({ color: 0x969100 });
-wireFrameMaterial.wireframe = true;
-
-const mesh = createMultiMaterialObject(geometry, [material, wireFrameMaterial]);
+const mesh = createMultiMaterialObject(geometry, [wireFrameMaterial, material]);
 scene.add(mesh);
 
 render();
 
 function render() {
+	trackballControls.update();
 	stats.update();
 	renderer.render(scene, camera);
 	requestAnimationFrame(render);
-	orbitControls.update();
 }
 
-function generatePoints(R, r, n) {
+function generatePoints(R, r) {
 	var points = [];
-	for (let i = 0; i < n; i++) {
+	for (let i = 0; i < 10000; i++) {
 		let x = random(-(R + r), R + r);
 		let y = random(-r, r);
 		let z = random(-(R + r), R + r);
@@ -100,24 +100,24 @@ function random(min, max) {
 	return Math.random() * (max - min) + min;
 }
 
-function pow2(x) {
-	return Math.pow(x, 2);
-}
-
 function filterPoints(R, r, points) {
-	let a, b;
 	let filteredPoints = [];
-	points.forEach((v) => {
-		a = pow2(pow2(v.x) + pow2(v.y) + pow2(v.z) + pow2(R) - pow2(r));
-		b = 4 * pow2(R) * (pow2(v.x) + pow2(v.z));
-		if (a - b <= 0) filteredPoints.push(v);
+	points.forEach((point) => {
+		let x2 = Math.pow(point.x, 2);
+		let y2 = Math.pow(point.y, 2);
+		let z2 = Math.pow(point.z, 2);
+		let R2 = Math.pow(R, 2);
+		let r2 = Math.pow(r, 2);
+		const torusCondition = Math.pow(x2 + y2 + z2 + R2 - r2, 2) - 4 * R2 * (x2 + z2) <= 0;
+		if (torusCondition) {
+			filteredPoints.push(point);
+		}
 	});
 	return filteredPoints;
 }
 
 function calcU(x, z, s) {
-	let phi = Math.atan2(z, x);
-	return ((phi + Math.PI) / (2 * Math.PI)) * s;
+	return ((Math.atan2(z, x) + Math.PI) / (2 * Math.PI)) * s;
 }
 
 function calcV(y, r) {
@@ -139,7 +139,7 @@ function initStats() {
 }
 
 function createMultiMaterialObject(geometry, materials) {
-	var group = new THREE.Group();
+	var group = new THREE.Object3D();
 	for (let i = 0; i < materials.length; i++) {
 		group.add(new THREE.Mesh(geometry, materials[i]));
 	}
